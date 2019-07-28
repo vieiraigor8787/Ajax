cb111d4f194c13516f30613dd02a343432e058ec
Igor Cerqueira <igor_vc@hotmail.com>
2019-07-28 19:22:27 -0300

    config upload

--- server.js
+++ server.js
@@ -6,5 +6,26 @@ app.use(exp.static('.'))//middleware> dentro da pasta atual onde está o server.
 app.use(bB.urlencoded({extended: true}))//lê os dados do formato de formulário através de um submit e transforma em objeto
 app.use(bB.json())//código aplicado que transforma o Json em objeto
 
-app.get('/teste', (req, res) => res.send('Ok'))
+const multer = require('multer')//interpretar formulário upload
+//configurando armazenamento
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')//parâmetro de definição de armazenamento do arquivo, no caso aqui, vai armazenar na pasta atual
+    },
+    filename: function (req, file, callback) {//escolher o nome do arquivo padrão
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => { //calback
+        if (err) {
+            return res.end('ocorreu um erro.')
+        }
+        res.end('concluído com sucesso')
+    })
+})
+
 app.listen(8080, () => console.log('executando...'))
