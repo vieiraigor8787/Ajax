2e5aaaea44f00a8378fb8eaa5350cc8a356b2dd2
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-29 15:47:21 -0300

    axios-get-post

--- server.js
+++ server.js
@@ -35,4 +35,11 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOimpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par?  'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('executando...'))
